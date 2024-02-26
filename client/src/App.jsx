12817061ae2e0b1e32afaf96eff922c308583ed2
Ankit Vishwakarma <ankit.coolzzzz@gmail.com>
2024-02-26 22:44:42 +0530

    bug fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import {UserContextProvider} from "./UserContext";
 import Routes from "./Routes";
 
 function App() {
-  axios.defaults.baseURL = 'http://localhost:8080';
+  axios.defaults.baseURL = 'http://localhost:8855';
   axios.defaults.withCredentials = true;
   return (
     <UserContextProvider>
