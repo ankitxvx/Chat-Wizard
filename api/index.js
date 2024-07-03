c7155ccd6e7df5865e32c058388a4289b25e11b8
Ankit Vishwakarma <ankit99aug@outlook.com>
2024-07-04 00:56:35 +0530

    make it better

--- api/index.js
+++ api/index.js
@@ -45,7 +45,9 @@ async function getUserDataFromRequest(req) {
     }
   });
 }
-
+app.get('/',(req,res)=>{
+  res.send("Hello World");
+});
 app.get('/test', (req, res) => {
   res.json('test ok');
 });
